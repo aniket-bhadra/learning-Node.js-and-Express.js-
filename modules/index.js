c1e580c5902dce903ad450475e37e7d8d1679703
aniket-bhadra <aniketbhadra98320@gmail.com>
2022-12-26 21:24:52 +0530

    exported & imported varibles & functions

---
+++ modules/index.js
@@ -0,0 +1,14 @@
+const printingNames = (names) => {
+    console.log(names);
+}
+
+const name = require('./names')
+
+// printingNames(name.name1);
+// printingNames(name.name2);
+
+// console.log(name);
+
+module.exports = printingNames;
+console.log(module);
+
---
+++ modules/names.js
@@ -0,0 +1,8 @@
+const name1 = "henry";
+const name2 = "peter";
+
+module.exports = {
+    name1, name2
+}
+
+// console.log(module);
