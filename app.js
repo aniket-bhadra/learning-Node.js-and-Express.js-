b40f63b4d58c38be01803b1b2f9922f0dc9a2b50
aniket-bhadra <aniketbhadra98320@gmail.com>
2022-12-27 18:32:42 +0530

    learned external modules & package.json & package-lock.json

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const _ = require("lodash");
+// const a = require("lodash");
 
+const items = [1, [2, 3, [3, [4]]]];
+
+const newItmes = _.flattenDeep(items);
+
+console.log(newItmes);
+console.log("heellssso");
