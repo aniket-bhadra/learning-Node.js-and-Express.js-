1a2b8e6a80f10221f3c5bd5d386610876b79c543
aniket-bhadra <aniketbhadra98320@gmail.com>
2022-12-27 12:38:58 +0530

    learned http module basics

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.write("welcome to our web page");
-  res.end();
-});
-
-
-//now define the port which server should listen to
-server.listen(5000);
