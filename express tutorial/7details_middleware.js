841b586b3b5a15e290fd438ee11d142d8b1577d3
aniket-bhadra <aniketbhadra98320@gmail.com>
2022-12-31 23:52:50 +0530

    working with third party middleware

--- express tutorial/7details_middleware.js
+++ express tutorial/7details_middleware.js
@@ -43,4 +43,33 @@
 //   console.log("server is listening on 5000......");
 // });
 
+//--------------------------------------------------for third party middleware--------------------------------------------
+const express = require("express");
+const app = express();
 
+//third party middleware
+const morgan = require("morgan");
+
+app.use(morgan("tiny"));
+
+
+
+app.get("/", (req, res) => {
+  res.send("home page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("about page");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("product page");
+  console.log(req.user);
+});
+app.get("/api/items", (req, res) => {
+  res.send("item page");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on 5000......");
+});
