0e4670f666ca41071816f71c2ba431c91c1e90da
aniket-bhadra <aniketbhadra98320@gmail.com>
2022-12-31 23:49:14 +0530

    working with authorize middleware function

---
+++ express tutorial/7details_middleware.js
@@ -0,0 +1,31 @@
+// const express = require("express");
+// const app = express();
+// const logger = require("./logger");
+// const authorize = require("./authorize");
+
+// app.use([logger, authorize]);
+
+// app.get("/", (req, res) => {
+//   res.send("home page");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("about page");
+// });
+
+// app.get("/api/products",[logger, authorize], (req, res) => {
+//   res.send("product page");
+//   console.log(req.user);
+// });
+// app.get("/api/items", (req, res) => {
+//   res.send("item page");
+// });
+
+
+
+
+// app.listen(5000, () => {
+//   console.log("server is listening on 5000......");
+// });
+
+
