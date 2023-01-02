3472b7e9de2faa699cceb423d39dd1589d965d43
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-02 23:23:12 +0530

    setting up post method

--- express tutorial/allrequests.js
+++ express tutorial/allrequests.js
@@ -8,3 +8,10 @@ app.get("/api/people", (req, res) => {
     // console.log("inside get method");
     res.status(200).json({ success: true, data: people }); //this how we send the json data
   });
+
+
+//post method
+//add data onto server
+//1. setup static assets (coz to send post request either we have to use postman tool or in client side have implement a form or have to call api, we can't configure browser to send post request)
+
+app.use(express.static("./public"));
