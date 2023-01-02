d5dc3d8035daa44c2c275074e7404f8df9e9e58e
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-02 23:36:43 +0530

    learned how to handle DELETE request

--- express tutorial/allrequests.js
+++ express tutorial/allrequests.js
@@ -102,7 +102,7 @@ app.post("/api/postman/people", (req, res) => {
     res.status(201).json({ success: true, data: [...people, name] });
   });
 
-  
+
 
   //12.put method
 //by convention in put method , we provide the id in the url (basically we add that in route parameter) for the item to edit or update
@@ -126,6 +126,22 @@ app.put("/api/people/:id", (req, res) => {
   
   
   });
+
+  //13.delete method
+//setup very similar like put, just we don't expect any body here
+app.delete('/api/people/:id', (req,res)=> {
+    const { id } = req.params;
+    const updatedItem = people.find((element) => element.id === Number(id));
+  
+    if(updatedItem){
+      const deletedListofPeople = people.filter(person => person.id !== Number(id))
+     return res.status(200).json({success: true, data: deletedListofPeople})
+    }
+   
+    return res.status(404).json({success: false, msg: "your requestd id does not exist"})
+  
+  
+  })
 
 
 
