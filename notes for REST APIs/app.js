301396c69e627a1e363b36dc8950227933c9239a
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-06 23:49:58 +0530

    basic strucutre while setting up a REST API

---
+++ notes for REST APIs/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+
+require("dotenv").config();
+
+//handling the incomming json data with this middleware
+app.use(express.json());
+
+//handling the static files
+app.use(express.static("./public"));
+
+app.get("/hello", (req, res) => {
+  res.status(200).send("<h1>every thing is successfull</h1>");
+});
+
+//setting up the middleware for this route
+app.use("/api/v1/tasks", tasks);
+
+//these are the routes or endpoints that needs be define
+//app.get('api/v1/tasks')  ----------get all the tasks
+//app.post('api/v1/tasks')  ----------create a new task
+//app.get('api/v1/tasks/:id')  ----------get single task
+//app.patch('api/v1/tasks/:id')  ----------update task
+//app.delete('api/v1/tasks/:id')  ----------delete task
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_PATH);
+    app.listen(
+      port,
+      console.log(`server is listening to the port ${port}.....`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
