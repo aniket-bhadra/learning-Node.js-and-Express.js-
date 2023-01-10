f0cddd618c4aa698c56ef8c98184ff37d0f069f2
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-10 23:40:02 +0530

    createTask controller fun added

--- notes for REST APIs/controllers/tasks.js
+++ notes for REST APIs/controllers/tasks.js
@@ -25,6 +25,15 @@
 //   }
 // };
 
+// const createTask = async (req, res) => {
+//   try {
+//     const task = await Task.create(req.body);
+//     res.status(201).json({ task });
+//   } catch (error) {
+//     res.status(500).json({ msg: error });
+//   }
+// };
+
 
 
 // module.exports = {
