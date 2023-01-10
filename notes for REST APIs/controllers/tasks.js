73c5ae141ff5969b069f0b293bf77b2523850f22
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-10 23:24:17 +0530

    getAllTasks controller fun added

--- notes for REST APIs/controllers/tasks.js
+++ notes for REST APIs/controllers/tasks.js
@@ -1,43 +1,37 @@
-const Task = require("../models/Task");
 
-//these are the routes or endpoints that needs be define
-//app.get('api/v1/tasks')  ----------get all the tasks
-//app.post('api/v1/tasks')  ----------create a new task
-//app.get('api/v1/tasks/:id')  ----------get single task
-//app.patch('api/v1/tasks/:id')  ----------update task
-//app.delete('api/v1/tasks/:id')  ----------delete task
 
-//step-1---setup 1st controller
-const getAllTasks = (req, res) => {
-  res.send("get all tasks");
-};
+// const Task = require("../models/Task");
 
-//step-3
-//now setup the rest controllers for other routes
+// const getAllTasks = async (req, res) => {
+//   try {
+//     const tasks = await Task.find({});
+//     res.status(200).json({ tasks });
 
-const createTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });  //es7 syntax-- variable name & property name same
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+//     //while sending response, we can send diff type of response in any controller (getallTasks, createTask,...), ex--
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
-};
-const updateTask = (req, res) => {
-  res.send("update task");
-};
-const deleteTask = (req, res) => {
-  res.send("delete task");
-};
+//     //we can send here like this also--
+//     //1.
+//     //res.status(200).json({ tasks, amount: tasks.length });
+//     //2.
+//     // res
+//     //   .status(200)
+//     //   .json({ status: "success", data: { tasks, nbHits: tasks.length } });
 
-module.exports = {
-  getAllTasks,
-  createTask,
-  getTask,
-  updateTask,
-  deleteTask,
-};
+//     //etc etc
+//     //imp thing is just stay consistent while sending response in every route
+
+//   } catch (error) {
+//     res.status(500).json({ msg: error });
+//   }
+// };
+
+
+
+// module.exports = {
+//   getAllTasks,
+//   createTask,
+//   getTask,
+//   updateTask,
+//   deleteTask,
+// };
+
