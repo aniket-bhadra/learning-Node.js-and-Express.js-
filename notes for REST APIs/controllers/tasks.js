1603dc2f32e6c2e6f9d2cd2860188540e87b603a
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-10 23:41:17 +0530

    deleteTask controller fun added

--- notes for REST APIs/controllers/tasks.js
+++ notes for REST APIs/controllers/tasks.js
@@ -51,6 +51,26 @@
 //   }
 // };
 
+// const deleteTask = async (req, res) => {
+//   try {
+//     const { id: taskID } = req.params;
+//     const task = await Task.findOneAndDelete({ _id: taskID });
+//     if (!task) {
+//       return res
+//         .status(404)
+//         .json({ msg: `no task exist wih the id: ${taskID}` });
+//     }
+
+//     res.status(200).json({ task }); //here we sent that task that we r removed, for better understanding in postman
+
+//     //since mostly the delted item will not nedded in client side, so here we can send any success response ex-
+//     //res.status(200).send()
+//     //res.status(200).json({ task: null, status: "success" })
+//   } catch (error) {
+//     res.status(500).json({ msg: error });
+//   }
+// };
+
 
 
 // module.exports = {
