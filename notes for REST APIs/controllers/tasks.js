e543ffc359fda505be2d0cb94228097de652f472
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-12 23:20:31 +0530

    getTask for getting single item is refactored

--- notes for REST APIs/controllers/tasks.js
+++ notes for REST APIs/controllers/tasks.js
@@ -128,3 +128,19 @@ const createTask = asyncWrapper(async (req, res) => {
   const task = await Task.create(req.body);
   res.status(201).json({ task });
 });
+
+
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOne({ _id: taskID });
+
+  if (!task) {
+    // const error = new Error('Not Found');
+    // error.status = 404;
+    // return next(error); //return--coz i don't want js to keep reading the code after this line
+
+    //now refactored this above code-->
+    return next(createCustomError(`no task exist wih the id: ${taskID}`, 404));
+  }
+  res.status(200).json({ task });
+});
