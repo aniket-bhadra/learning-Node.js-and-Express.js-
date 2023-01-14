1587c9cecec0b924d872acf4c91717b2b186a063
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-14 23:08:35 +0530

    sette up port varaible & setting the server

--- advance REST API/app.js
+++ advance REST API/app.js
@@ -18,3 +18,22 @@ const productsRouter = require("./routes/products");
 //importing errorHandler middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
+
+
+
+
+//now setup the port. then connect to the database & start server
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //here connect to the database first & ONLY then start the server
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening port ${port}....`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
