a5579200ea21a37bfa0b3bfb4004b8e717612237
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-17 22:47:11 +0530

    getAllProductsStatic controller added for testing routes

---
+++ advance REST API/controllers/product.js
@@ -0,0 +1,6 @@
+//---------------------->>>>>>>>>>-----basic setup for testing routes & controller functions-----------------------
+
+// const getAllProductsStatic = async (req, res) => {
+//   throw new Error("testing async errors"); //since we r using express-async-error pacakge so insead of using next() here we just throw error. & this does the job for us. this error will be received by the error-handler.js
+//   res.status(200).json({ msg: "products testing route" });
+// };
