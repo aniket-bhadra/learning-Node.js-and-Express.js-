995ed1e2c391820a54632b5b7c8665309596958b
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-17 22:48:56 +0530

    getAllProducts controller added for testing routes

--- advance REST API/controllers/product.js
+++ advance REST API/controllers/product.js
@@ -4,3 +4,12 @@
 //   throw new Error("testing async errors"); //since we r using express-async-error pacakge so insead of using next() here we just throw error. & this does the job for us. this error will be received by the error-handler.js
 //   res.status(200).json({ msg: "products testing route" });
 // };
+
+// const getAllProducts = async (req, res) => {
+//   res.status(200).json({ msg: "products route" });
+// };
+
+// module.exports = {
+//   getAllProducts,
+//   getAllProductsStatic,
+// };
