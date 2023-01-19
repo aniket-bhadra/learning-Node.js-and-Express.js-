1d60eb13b4dbf0e9389757f8117a2b10bc243e54
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-19 22:36:21 +0530

    pagination implemented

--- advance REST API/controllers/product.js
+++ advance REST API/controllers/product.js
@@ -120,7 +120,12 @@ const getAllProductsStatic = async (req, res) => {
     result = result.select(fieldsList);
   }
   
-  
+    //limit & skip
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
   
   const products = await result;
   res.status(200).json({ nbHits: products.length, products });
