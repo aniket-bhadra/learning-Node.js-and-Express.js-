fcc8562f03d06edd620243de821f73eaf245713f
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-20 23:24:23 +0530

    integrated all the middlewares

--- JWT/app.js
+++ JWT/app.js
@@ -11,4 +11,13 @@ const mainRouter = require("./routes/main");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+app.use("/api/v1", mainRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 
