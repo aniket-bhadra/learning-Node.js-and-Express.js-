2b60cc9e2397b56ebe48300d98c32f112546ead0
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-22 17:14:37 +0530

    BadRequest class created

---
+++ JWT/errors/bad-request.js
@@ -0,0 +1,12 @@
+const CustomAPIError = require("./custom-error");
+const { StatusCodes } = require("http-status-codes");
+//since it is easier to read & understand Also with this we've consistency that is why using this package instead of hard coding 404, 404 ,...
+
+class BadRequest extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+module.exports = BadRquest;
