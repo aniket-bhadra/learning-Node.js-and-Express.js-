406941c9e58553ded22d2c4e69b23beeafbb7223
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-27 23:15:50 +0530

    key things need to perfom in controllers

---
+++ JWT/controllers/main.js
@@ -0,0 +1,5 @@
+// //1st check the username, password in post request(login)
+// //if exist then create new JWT
+// //send back to front-end
+// //now frontend gonna send get request with that JWT
+// //then we approve the request & send the autorize data
