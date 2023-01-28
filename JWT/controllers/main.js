1f1d1ba58774558d13cbda8552946a2d274d990b
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-01-28 23:10:14 +0530

    imported JWT package

--- JWT/controllers/main.js
+++ JWT/controllers/main.js
@@ -3,3 +3,11 @@
 // //send back to front-end
 // //now frontend gonna send get request with that JWT
 // //then we approve the request & send the autorize data
+
+/ ////if username,password not exist then send error
+// //so only the request with JWT can access the dashboard
+
+// //1st import the package
+// const jwt = require("jsonwebtoken");
+
+// const CustomAPIError = require("../errors/custom-error");
