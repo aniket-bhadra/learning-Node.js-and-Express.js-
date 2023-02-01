15f93c354c555f6b4ae2bdb2ac5ba871e6abd29b
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-01 23:30:04 +0530

    imported extra security packages

--- JOBS-API/app.js
+++ JOBS-API/app.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
 
-
+//importing extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 const express = require("express");
 const app = express();
