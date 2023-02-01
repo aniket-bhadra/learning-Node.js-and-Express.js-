2e900b38c74c9abd416bffcdcf7abfcbfc21abc1
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-01 23:37:19 +0530

    setted the port & started server

--- JOBS-API-notes/app.js
+++ JOBS-API-notes/app.js
@@ -24,3 +24,20 @@ const jobsRouter = require("./routes/jobs");
 // import error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+stat();
+
