0b1b4ac84f612e615fb8bc571ab7e965f7283372
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-02 23:19:00 +0530

    added all extra secuity middlewares

--- JOBS-API-notes/app.js
+++ JOBS-API-notes/app.js
@@ -25,6 +25,21 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+
+// using extra security packages
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 min
+    max: 100, 
+    standardHeaders: true, 
+    legacyHeaders: false, 
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // routes
 app.use("/api/v1/auth", authRouter);
 //2.then pass it infront of all those routes that should be protected
