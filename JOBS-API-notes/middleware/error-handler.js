e51a637531d8044a9852bca20778c9fdaadd4046
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-06 01:13:14 +0530

    Setted up a default error object

--- JOBS-API-notes/middleware/error-handler.js
+++ JOBS-API-notes/middleware/error-handler.js
@@ -3,3 +3,20 @@
 //-validation errors
 //- duplicate(email)
 //-- cast error
+
+ const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    //set default
+
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wront try again later",
+  };
+
+   
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
