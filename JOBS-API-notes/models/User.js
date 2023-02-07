d08f01d42c3142d69d30570f0869fa1a6a40718e
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-07 23:21:11 +0530

    mongoose middleware works same with out using next()

--- JOBS-API-notes/models/User.js
+++ JOBS-API-notes/models/User.js
@@ -35,6 +35,6 @@ UserSchema.pre("save", async function (next) {
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
-  
+  // next();
+  //if we go without next() functionality still going to works
 });
