65a7e3bdf0c4c09876cc7e87a5da7652a457ffd0
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-11 23:32:44 +0530

    hashing password setup done

--- JOBS-API-notes/controllers/auth.js
+++ JOBS-API-notes/controllers/auth.js
@@ -11,8 +11,30 @@ const register = async (req, res) => {
   }
 
   //since we want mongoose to do all the validation. thats why we directly passed req.body instead of validate here and then pass the validated object into the model //coz here specilly in this case its is optional to check in controller but in some cases we've to use both, here specifically in this case using only mongoose validator we can send more meaningful error response
+  
+  
+   //hashing password setup
+  const { name, email, password } = req.body;
+
+  //we've option to run them asynchronously thats why go with await
+  const salt = await bcrypt.genSalt(10);
+
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = {
+    name,
+    email,
+    password: hashedPassword,
+  };
+
+  const user = await User.create({ ...tempUser });
+
+res.status(StatusCodes.CREATED).json({ user });
+};
   res.send("registered user");
 };
+
+
 
 const login = async (req, res) => {
   res.send("login user");
