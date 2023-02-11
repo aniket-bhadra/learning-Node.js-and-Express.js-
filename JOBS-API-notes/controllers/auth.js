d478efd61c964e41c296dd54ba4ca8479ed982b1
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-11 23:29:11 +0530

    checking emptyvalues inside register controller

--- JOBS-API-notes/controllers/auth.js
+++ JOBS-API-notes/controllers/auth.js
@@ -5,6 +5,12 @@ const bcrypt = require("bcryptjs");
 
 
 const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please provide name, email, and password");
+  }
+
+  //since we want mongoose to do all the validation. thats why we directly passed req.body instead of validate here and then pass the validated object into the model //coz here specilly in this case its is optional to check in controller but in some cases we've to use both, here specifically in this case using only mongoose validator we can send more meaningful error response
   res.send("registered user");
 };
 
