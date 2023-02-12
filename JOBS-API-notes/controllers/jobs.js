b02bf4e4b0a08e8eca638e6e414ff30cdcafd978
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-13 02:27:13 +0530

    controller getAllJobs created

--- JOBS-API-notes/controllers/jobs.js
+++ JOBS-API-notes/controllers/jobs.js
@@ -1,3 +1,11 @@
 const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
+
+const getAllJobs = async (req, res) => {
+  //in this case we r not looking for all the jobs like previous projects (ex-tasks), here we r only looking for jobs that are associated with this user
+
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+
+  res.status(StatusCodes.OK).json({ count: jobs.length, jobs });
+};
