0ddd01c7bcb56213a40941d672f05f2071794cd2
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-12 22:47:36 +0530

    Refactored theregister controller

--- JOBS-API-notes/controllers/auth.js
+++ JOBS-API-notes/controllers/auth.js
@@ -54,3 +54,16 @@
 
 //----------------------------------------..------------>>>>>>>>>>>>---all the testing has been completed now refactored the above code with alredy defined mongoose middleware
 
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+};
+
+
