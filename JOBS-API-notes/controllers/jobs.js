47bcee3ad8b2f5846ecbeebfca97b86727fca9ea
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-13 02:29:25 +0530

    updateJob controller created

--- JOBS-API-notes/controllers/jobs.js
+++ JOBS-API-notes/controllers/jobs.js
@@ -31,3 +31,27 @@ const createJob = async (req, res) => {
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
+const updateJob = (req, res) => {
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === "" || position === "") {
+    throw new BadRequestError("Company or Position field cannot be empty");
+  }
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  if (!job) {
+    // console.log(job)
+    throw new NotFoundError(`no job with the ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
+};
