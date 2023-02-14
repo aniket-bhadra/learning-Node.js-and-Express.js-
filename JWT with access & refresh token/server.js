3eac3b3f82f332a058db01f83e1674231629dd6a
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-15 00:00:59 +0530

    authenticateToken middleware added

--- JWT with access & refresh token/server.js
+++ JWT with access & refresh token/server.js
@@ -23,7 +23,23 @@ const posts = [
   },
 ];
 
+app.get("/posts", (req, res) => {
+  res.json({name: "hey" });
+});
 
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token === null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(6000, () => {
   console.log("server is listening to port 6000");
