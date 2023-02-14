e0cf5906fd36c0de4e5346b7af180e7f7c844d92
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-15 01:06:40 +0530

    asynchrouns js revision

---
+++ JWT with access & refresh token/testing.js
@@ -0,0 +1,27 @@
+const getData = () => {
+  return new Promise((resolve, reject) => {
+    const value = false;
+    setTimeout(() => {
+      if (value) {
+        resolve("yes");
+      } else {
+        reject("no");
+      }
+    }, 5000);
+  });
+};
+
+const checking = () => {
+  try {
+    data = await getData();
+    return data;
+  } catch (error) {
+    // console.log(error);
+    return error;
+  }
+};
+async function jo() {
+  console.log(await checking());
+}
+console.log("this is outside", checking());
+jo();
