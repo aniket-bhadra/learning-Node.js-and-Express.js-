1d3780ec540f189fc2e34f5fc7beed66ab800e41
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-14 23:56:17 +0530

    imported all the post data

--- JWT with access & refresh token/server.js
+++ JWT with access & refresh token/server.js
@@ -7,6 +7,24 @@ const app = express();
 const jwt = require("jsonwebtoken");
 app.use(express.json());
 
+
+const posts = [
+  {
+    username: "jim",
+    title: "yea new thing",
+  },
+  {
+    username: "smith",
+    title: "i love ginger",
+  },
+  {
+    username: "carry",
+    title: "yes it is...",
+  },
+];
+
+
+
 app.listen(6000, () => {
   console.log("server is listening to port 6000");
 });
