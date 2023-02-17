68535c3e7055478f136ca2e5c75c92d27b84e393
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-17 19:05:21 +0530

    basic server setup is done

--- JWT with access & refresh token/authServer.js
+++ JWT with access & refresh token/authServer.js
@@ -1,3 +1,13 @@
 //this server only for authentication & authorization
 //this server only going to  used for login logut
 //& for creation of token, deletion of token & refrshen of token
+
+const express = require("express");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+app.use(express.json());
+
+app.listen(4000, () => {
+  console.log("server is listening to port 4000");
+});
