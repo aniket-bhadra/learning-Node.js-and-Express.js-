71934c381f1609297787d33680c2631b262e2be3
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-17 19:12:50 +0530

    imp thins to keep in mind while issuing new token

--- JWT with access & refresh token/authServer.js
+++ JWT with access & refresh token/authServer.js
@@ -17,7 +17,19 @@ app.post("/token", (req, res) => {
   const refreshToken = req.body.token;
   if (!refreshToken) return res.sendStatus(401);
   if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  // here we r checking--------the token that we r getting from the request with token that we have in you valid list of refresh tokens, so if that checks get passed why we r veirfying in the next step?
+
+
+
+
+  //normally this refreshToken save into your database, so if somebody get illegal access to your db & also stolen valid user refresh token still they can't generate the access token with overwritten data coz of this extra verify step, if they change the token data & then create JWT & then store that token to your db & replace the your existing refreshToken, still they will be failed to generate vaild accessToken from that.
   
+   //coz after getting of all still that person needs to know the secret key to generate the same signature, only then they can genrate valid jwt with overwritten data
+  
+  //& also because of that extra step , if somebody get illegal access to you refresh token, but when that refresh token expired OR when we ourself deleted that refresh token after any action (for ex- logout), hacker can't issue another valid refresh token to genrate access tokens, coz hacker does not have that secret key to genreate valid refresh token, this why verifying is imp
+
+ 
+
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_KEY, (error, user) => {
     if (error) return res.sendStatus(403);
     const accessToken = generateAccessToken({ name: user.name });
